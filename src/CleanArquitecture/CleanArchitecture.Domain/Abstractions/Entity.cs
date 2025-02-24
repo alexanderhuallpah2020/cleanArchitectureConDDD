3aa6b25fc6f9cd528944837346851a3334d22267
ajhuallpadcp <ajhuallpa@dcp.pe>
2025-02-24 12:47:59 -0500

    Modelando con DDD

---
+++ src/CleanArquitecture/CleanArchitecture.Domain/Abstractions/Entity.cs
@@ -0,0 +1,16 @@
+namespace ClearArchitecture.Domain.Abstractions;
+
+public abstract class Entity
+{
+    protected Entity(Guid id)
+    {
+        Id = id;
+    }
+
+    public Guid Id {get; init;}
+}
+
+
+
+
+
